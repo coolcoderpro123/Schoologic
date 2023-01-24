25b738170d69ec578bb9db5390d1b078d44c5a40
coolcoderpro123 <117481159+coolcoderpro123@users.noreply.github.com>
2023-01-24 12:39:34 -0800

    Update adminpage.js

--- adminpage.js
+++ adminpage.js
@@ -1,1 +1,15 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
+  if (username === "eagleadmin" && password === "adminsm417") {
+    var success = document.getElementById("success");
+    success.innerHTML = "Successful Login";
+    setTimeout(() => {
+        window.location.href = "success.html";
+    },2000);
+  } else {
+    var error = document.getElementById("error");
+    error.innerHTML = "Invalid username or password. Please try again.";
+  }
+}
